148e8ef603b673729cd1e40856bf98469fc0ecbb
Roberto Padilla <rovertopadilla56@gmail.com>
2023-09-01 17:13:36 -0600

    Añadiendo lista opciones

--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -1,5 +1,6 @@
 import "./Formulario.css";
 import CampoTexto from "../CampoTexto/CampoTexto";
+import ListaOpciones from "../ListaOpciones";
 
 const Formulario = () => {
   return (
@@ -9,6 +10,7 @@ const Formulario = () => {
         <CampoTexto titulo="Nombre" placeholder="Ingresar nombre"/>
         <CampoTexto titulo="Puesto" placeholder="Ingresar puesto" />
         <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto"/>
+        <ListaOpciones/>
       </form>
     </section>
   );
