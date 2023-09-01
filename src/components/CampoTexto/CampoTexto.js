25da9db090a6be794a8f61bdc27ade0bd92da1db
Roberto Padilla <rovertopadilla56@gmail.com>
2023-09-01 16:41:45 -0600

    Agregando archivos Campo texto

---
+++ src/components/CampoTexto/CampoTexto.js
@@ -0,0 +1,13 @@
+import "./CampoTexto.css";
+
+const CampoTexto = (props) => {
+  const placeHolderModificado = `${props.placeholder}...`
+  return (
+    <div className="campo-texto">
+      <label>{props.titulo}</label>
+      <input placeholder={placeHolderModificado} />
+    </div>
+  );
+};
+
+export default CampoTexto;
