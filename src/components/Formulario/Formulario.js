e3f1865e5ce77987e7e1f45c9e525c7daddb1579
Roberto Padilla <rovertopadilla56@gmail.com>
2023-09-01 16:40:34 -0600

    Finalizando Campo Texto

--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -1,10 +1,14 @@
 import "./Formulario.css";
+import CampoTexto from "../CampoTexto/CampoTexto";
 
 const Formulario = () => {
   return (
     <section className="formulario">
       <form>
         <h2>Rellena el formulario para crear el colaborador</h2>
+        <CampoTexto titulo="Nombre" placeholder="Ingresar nombre"/>
+        <CampoTexto titulo="Puesto" placeholder="Ingresar puesto" />
+        <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto"/>
       </form>
     </section>
   );
