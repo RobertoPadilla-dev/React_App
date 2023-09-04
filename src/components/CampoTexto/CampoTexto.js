81a24900a447e595f5336e9eefede1ae456ffca5
Roberto Padilla <rovertopadilla56@gmail.com>
2023-09-04 17:34:25 -0600

    Actualizando componentes

--- src/components/CampoTexto/CampoTexto.js
+++ src/components/CampoTexto/CampoTexto.js
@@ -1,11 +1,23 @@
+import { useCallback, useState } from "react";
 import "./CampoTexto.css";
 
 const CampoTexto = (props) => {
-  const placeHolderModificado = `${props.placeholder}...`
+  const [valor, actualizarValor] = useState();
+  const placeHolderModificado = `${props.placeholder}...`;
+
+  const manejarCambio = (e) => {
+    props.actualizarValor(e.target.value)
+  };
+
   return (
     <div className="campo-texto">
       <label>{props.titulo}</label>
-      <input placeholder={placeHolderModificado} required={props.required}/>
+      <input
+        placeholder={placeHolderModificado}
+        required={props.required}
+        value={props.valor}
+        onChange={manejarCambio}
+      />
     </div>
   );
 };
--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -2,21 +2,54 @@ import "./Formulario.css";
 import CampoTexto from "../CampoTexto/CampoTexto";
 import ListaOpciones from "../ListaOpciones";
 import Boton from "../Boton";
+import { useState } from "react";
 
 const Formulario = () => {
+  const [nombre, actualizarNombre] = useState("");
+  const [puesto, actualizarPuesto] = useState("");
+  const [foto, actualizarFoto] = useState("");
+  const [equipo, actualizarEquipo] = useState("");
+
   const manejarEnvio = (e) => {
     e.preventDefault();
-    console.log("Manejar envio", e);
+    console.log("Manejar envio");
+    let datosAEnviar = {
+      nombre,
+      puesto,
+      foto,
+      equipo
+    }
   };
 
   return (
     <section className="formulario">
       <form onSubmit={manejarEnvio}>
         <h2>Rellena el formulario para crear el colaborador</h2>
-        <CampoTexto titulo="Nombre" placeholder="Ingresar nombre" required/>
-        <CampoTexto titulo="Puesto" placeholder="Ingresar puesto" required/>
-        <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto" required/>
-        <ListaOpciones />
+        <CampoTexto
+          titulo="Nombre"
+          placeholder="Ingresar nombre"
+          required
+          valor={nombre}
+          actualizarValor={actualizarNombre}
+        />
+        <CampoTexto 
+        titulo="Puesto" 
+        placeholder="Ingresar puesto" 
+        required
+        valor={puesto}
+        actualizarValor={actualizarPuesto} 
+        />
+        <CampoTexto
+          titulo="Foto"
+          placeholder="Ingresar enlace de foto"
+          required
+          valor={foto}
+          actualizarValor={actualizarFoto}
+        />
+        <ListaOpciones 
+        valor={equipo}
+        actualizarEquipo={actualizarEquipo}
+        />
         <Boton>Crear</Boton>
       </form>
     </section>
