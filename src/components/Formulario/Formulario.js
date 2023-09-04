6dfd07869eb24a15d10f7dad29019fc8e814887e
Roberto Padilla <rovertopadilla56@gmail.com>
2023-09-04 09:31:17 -0600

    Agregando componente boton

--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -1,6 +1,7 @@
 import "./Formulario.css";
 import CampoTexto from "../CampoTexto/CampoTexto";
 import ListaOpciones from "../ListaOpciones";
+import Boton from "../Boton";
 
 const Formulario = () => {
   return (
@@ -11,6 +12,9 @@ const Formulario = () => {
         <CampoTexto titulo="Puesto" placeholder="Ingresar puesto" />
         <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto"/>
         <ListaOpciones/>
+        <Boton>
+          Crear
+        </Boton>
       </form>
     </section>
   );
